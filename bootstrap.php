8f69c7e57cbdd6f3aa3cef4245e0d23635ff0fd3
Jacob Emerick <jpemeric@gmail.com>
2014-10-13 06:37:55 -0500

    add in basic, non-functional, followers cron

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,20 @@
 
 require_once 'vendor/autoload.php';
 
+use GuzzleHttp\Client,
+    GuzzleHttp\Subscriber\Oauth\Oauth1;
+
+$oauth = new Oauth1([
+    'consumer_key'     => 'my_key',
+    'consumer_secret'  => 'my_secret',
+    'token'            => 'my_token',
+    'token_secret'     => 'my_token_secret',
+]);
+
+$rest_client = new Client([
+    'base_url'  => 'https://api.twitter.com/1.1/',
+    'defaults'  => ['auth' => 'oauth'],
+]);
+
+$rest_client->getEmitter()->attach($oauth);
+
---
+++ script/cron/fetch-followers.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+$result = $rest_client->get('followers/ids.json');
+// do something with the result
+
