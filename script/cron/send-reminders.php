0080071db577e4e58e791c884a0d53900d3c1a25
Jacob Emerick <jpemeric@gmail.com>
2015-01-01 16:04:45 -0700

    bah, uniqueness string for the twitter bot nazis

--- script/cron/send-reminders.php
+++ script/cron/send-reminders.php
@@ -93,7 +93,8 @@ while (($follower = $statement->fetch(PDO::FETCH_OBJ)) != false) {
     }
 
     // send reminder to do pushups
-    $tweet = "@{$follower->screen_name} time for pushups!";
+    $time = date('g:i a');
+    $tweet = "@{$follower->screen_name} it's {$time}... Time for pushups!";
     try {
         $result = $rest_client->post('statuses/update.json', [
             'body' => [
