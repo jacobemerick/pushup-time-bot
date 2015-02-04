be04d6fdfd66e5662ae54fb6c2ab7b9b564a2cd2
Jacob Emerick <jpemeric@gmail.com>
2015-02-04 04:28:28 -0700

    load up config from external, gitignored file, a more sane way to do it

--- bootstrap.php
+++ bootstrap.php
@@ -1,28 +1,41 @@
 <?php
 
-date_default_timezone_set('America/Chicago');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$handle = @fopen('config.json', 'r');
+if ($handle == false) {
+    exit('No configuration found.');
+}
+$config = fread($handle, filesize('config.json'));
+fclose($handle);
+$config = @json_decode($config, true);
+if (json_last_error() !== JSON_ERROR_NONE) {
+    exit('Could not read configuration.');
+}
+
+date_default_timezone_set($config['timezone']);
+$bot_name = $config['bot_name'];
 
 require_once 'vendor/autoload.php';
 
 use Aura\Sql\ExtendedPdo;
 
 $pdo = new ExtendedPdo(
-    'connect-string',
-    'username',
-    'password'
+    $config['database']['connection'],
+    $config['database']['username'],
+    $config['database']['password']
 );
 
 use GuzzleHttp\Client,
     GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 $oauth = new Oauth1([
-    'consumer_key'     => 'my_key',
-    'consumer_secret'  => 'my_secret',
-    'token'            => 'my_token',
-    'token_secret'     => 'my_token_secret',
+    'consumer_key'     => $config['twitter']['consumer_key'],
+    'consumer_secret'  => $config['twitter']['consumer_secret'],
+    'token'            => $config['twitter']['token'],
+    'token_secret'     => $config['twitter']['token_secret'],
 ]);
-
-$bot_name = 'pushuptime';
 
 $rest_client = new Client([
     'base_url'  => 'https://api.twitter.com/1.1/',
